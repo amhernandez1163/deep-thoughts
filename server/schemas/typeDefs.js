a6cb89d7accc9632b4c312047ab1c03e3d3f23ff
Amanda Dell <amhernandez1163@gmail.com>
2022-06-30 10:20:08 -0500

    updated mutations and are now working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,6 @@
-// import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
-// create our typeDefs
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
   type User {
     _id: ID
     username: String
@@ -21,6 +10,15 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
   type Reaction {
     _id: ID
     reactionBody: String
@@ -28,13 +26,26 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
